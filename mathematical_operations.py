15819e547b8a021963eb5b43788c225b4b30a48d
Jenil Ambaliya <Jenilambaliya555@gmail.com>
2023-01-28 19:02:17 +0530

    Feat: Variable Types and Basic Operators

---
+++ mathematical_operations.py
@@ -0,0 +1,41 @@
+a = 10
+b = 20
+
+# Mathematical Operators
+'''
+    Addition :- +
+    Subtraction :- -
+    Multiplication :- *
+    Division :- /
+    Modulus :- %
+    Exponential :- **
+    Floor Division :- //
+'''
+
+# Addition
+x = a + b
+print(x)
+
+# Subtraction
+y = a - b
+print(y)
+
+# Multiplication
+z = a * b
+print(z)
+
+# Division
+s = a / b
+print(s)
+
+# Modulus
+d = a % b
+print(d)
+
+# Exponential
+p = a ** b
+print(p)
+
+# Division Answer Without Decimal (Floor Division)
+m = b // a
+print(m)
---
+++ variables.py
@@ -0,0 +1,25 @@
+# Variable Types
+
+# Integer
+x = 1
+
+# Float
+y = 1.0
+
+# Complex
+z = 1j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Convert From Int To Float
+a = float(x)
+# Convert From Float To Int
+b = int(y)
+# Convert To Complex
+c = complex(x)
+
+print(a)
+print(b)
+print(c)
