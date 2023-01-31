2a7aa7573c21cc7a3515e580ec2975c7ddd794a2
Jenil Ambaliya <Jenilambaliya555@gmail.com>
2023-01-31 20:08:03 +0530

    Feat: Debugging

---
+++ debugging.py
@@ -0,0 +1,29 @@
+import random
+
+
+def GenerateRandom(upper):
+    r = random.randint(1, upper)
+    return r
+
+
+def main():
+    program = True
+    num1 = GenerateRandom(10)
+    num2 = GenerateRandom(10)
+    result = num1 * num2
+    while program:
+        ans = input("What is " + str(num1) + " x " + str(num2) + " = ")
+
+        if ans.isdigit():
+            if int(ans) == result:
+                print("Correct")
+                program = False
+            else:
+                print("Incorrect")
+        else:
+            print("Answer must be Positive")
+
+
+x = 10
+for x in range(x):
+    main()
