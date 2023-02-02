6f1fb5eaf6144efef1c18e03f12c9ec4134373f9
Jenil Ambaliya <Jenilambaliya555@gmail.com>
2023-02-02 15:12:34 +0530

    Feat: Iteration

---
+++ iterators.py
@@ -0,0 +1,10 @@
+# Difference Between Iterator and Interable
+# Iterable : List, Tuple and Dictionary
+# Iterator : Item of List, Tuple and Dictionary
+
+tuple1 = ("car", "bike", "train")
+myit = iter(tuple1)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
